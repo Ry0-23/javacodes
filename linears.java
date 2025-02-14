ceea7ee9ec29b0d8d1d97c20c11e0938242695fe
Ryou-asuka <harrypotter4548@gmail.com>
2025-02-14 11:14:24 +0530

    linear search program

--- linears.java
+++ linears.java
@@ -1,9 +1,42 @@
 import java.util.Scanner;
+
 public class linears {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int arr[] = scanner.nextInt();
-        System.out.print("enter the array :");
+        
+        // Get array size
+        System.out.print("Enter the size of array: ");
+        int size = scanner.nextInt();
+        
+        // Create array
+        int[] array = new int[size];
+        
+        // Input array elements
+        System.out.print("Enter the array elements: ");
+        for(int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
+        // Get search element
+        System.out.print("Enter the element to search: ");
+        int search = scanner.nextInt();
+        
+        // Perform linear search
+        int position = -1;
+        for(int i = 0; i < size; i++) {
+            if(array[i] == search) {
+                position = i;
+                break;
+            }
+        }
+        
+        // Display result
+        if(position != -1) {
+            System.out.println("Element " + search + " found at position: " + (position + 1));
+        } else {
+            System.out.println("Element not found in the array");
+        }
+        
+        scanner.close();
     }
-    
 }
